ef20f73290000167a5acd05ef17c98103e1910a5
Ahmad Akmal Amran <ranaeza21@gmail.com>
2025-04-07 21:55:19 +0700

    tambahan

---
+++ Soal1.js
@@ -0,0 +1,18 @@
+function hitung(rating){
+    const rating_terendah = Math.min(...rating);
+    const rating_tertinggi = Math.max(...rating);
+
+    const total = rating.reduce((acc, val) => acc + val, 0);
+    const rata_rata = total / rating.length;
+
+    hasil = [rating_terendah, rating_tertinggi, rata_rata];
+    return hasil;
+}
+
+const rating_contoh = [4.5, 2.0, 1.5, 3.0, 2.5, 4.0, 5.0, 3.5, 2.0, 1.0] 
+console.log("Input Contoh: " + rating_contoh);
+console.log("Output: " + hitung(rating_contoh));
+
+const rating_1 = [5,4,2.5,5,3.6,1.1,3.6,4,4.2,1.5]
+console.log("\nInput 1: " + rating_1);
+console.log("Output: " + hitung(rating_1));
